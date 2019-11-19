1af5e2d70211557355590cf6fea19f14a216506a
重田 将人 <masato.0928.j@gmail.com>
2019-11-20 08:47:30 +0900

    送信ボタン押下時のawsクルデンシャルエラー対応 #13

--- frontend/common/s3.js
+++ frontend/common/s3.js
@@ -2,10 +2,18 @@ import { Promise, resolve, reject } from 'q';
 
 export {uploadToS3}
 
+const aws = require("aws-sdk")
+
+aws.config.update({
+region: process.env.AWS_S3_REGION,
+secretAccessKey:process.env.AWS_SECRET_ACCESS_KEY,
+accessKeyId: process.env.AWS_ACCESS_KEY
+})
+
+const s3 = new aws.S3()
+
 // S3upload関数
 function  uploadToS3(data, path, bucket){
-    const aws = require("aws-sdk")
-    const s3 = new aws.S3()
 
     const params = {
         Body: data,
@@ -13,11 +21,6 @@ function  uploadToS3(data, path, bucket){
         Key: path, // TODO バケット/日付/ファイル名
     }
 
-    aws.config.update({
-        region: process.env.AWS_S3_REGION,
-        secretAccessKey:process.env.AWS_SECRET_ACCESS_KEY,
-        accessKeyId: process.env.AWS_ACCESS_KEY
-    })
     return new Promise((resolve, reject)=>{
         s3.upload(params, (err, data) => {
             if(err){
