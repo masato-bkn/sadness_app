37013983471704a24aa9ce99ee78280819b56c6c
重田 将人 <masato.0928.j@gmail.com>
2019-11-24 11:01:50 +0900

    [add] フロントエンドのテスト環境構築 #19

---
+++ frontend/test/specs/post.spec.js
@@ -0,0 +1,10 @@
+import { mount } from '@vue/test-utils'
+import Vuex from 'vuex'
+import Post from "~/components/post.vue";
+
+describe('post.vue', () => {
+    let wrapper
+    test('セットアップの確認', () => {
+        expect(true).toBe(true)
+    })
+})
