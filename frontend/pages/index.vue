ce4d9b0558e5890786ccb62a32df1f250b5a9f81
重田 将人 <masato.0928.j@gmail.com>
2019-12-03 21:16:08 +0900

    [clean] イベント名をケバブケースに変更 #33

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -3,11 +3,11 @@
     <div class="row" style="height: 850px;">
       <div class="col-md-7 col-sm-12">
         <Post v-if="images.length == 0"
-          @setOriginalImage="setOriginalImage"/>
+          @post-image="setOriginalImage"/>
         <Option v-else-if="images.length >= 2 & selected == false"
           :originalImage="originalImage"
-          @selectDone="setSelected"
-          @setSelectedImage="setSelectedImage"
+          @image-selection-complete="setSelected"
+          @select-image="setSelectedImage"
           />
         <Result v-else
           :image="selectedImage == '' ? images[0] : selectedImage"
