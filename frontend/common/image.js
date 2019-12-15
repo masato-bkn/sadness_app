ae26aa076050339c69a8077237947ce1f1b593cf
重田 将人 <masato.0928.j@gmail.com>
2019-12-15 14:03:11 +0900

    [update] canvasが枠からはみ出ないように修正 #51

--- frontend/common/image.js
+++ frontend/common/image.js
@@ -33,37 +33,12 @@ function resizeCanvas(canvas,image,canvasHeight) {
 
     // 画像を表示枠に収まるように縦横比を維持したままリサイズする。
     const maxHeight = 380
-    const maxWidth = 420
+    const maxWidth = 200
 
     let dh // イメージを描画する幅
     let dw // イメージを描画する高さ
 
-    // 縦幅が最大値を超えている場合
-    if (image.height >= maxHeight) {
-        // 縦比率にイメージの縦・横を合わせる
-        dh = maxHeight
-        dw = image.width * (dh / image.height)
-        // canvasの大きさを指定
-        canvas.width = dw
-        canvas.height = dh
-
-        // サムネイルに画像を描画
-        ctx.drawImage(image, 0, 0, image.width, image.height, 0, 0, dw, dh)
-    } 
-
-    // 横幅が最大値を超えている場合    
-    if (image.width >= maxWidth) {
-        // 横比率にイメージの縦・横を合わせる
-        dw = maxWidth
-        dh = image.height * (maxWidth / image.width)
-
-        // canvasの大きさを指定
-        canvas.height = dh
-        canvas.width = dw
-        // サムネイルに画像を描画
-        ctx.drawImage(image, 0, 0, image.width, image.height, 0, 0, dw, dh)
-    }
-    // どちらでもない場合は横に比率を合わせる
+    // maxより小さい場合はそのまま
     if (image.width < maxWidth && image.height < maxHeight) {
         // 横比率にイメージの縦・横を合わせる
         dw = maxWidth
@@ -74,6 +49,31 @@ function resizeCanvas(canvas,image,canvasHeight) {
         canvas.width = dw
         // サムネイルに画像を描画
         ctx.drawImage(image, 0, 0, image.width, image.height, 0, 0, dw, dh)
+        return
     }
+
+    // 縦幅が最大値を超えている場合
+    if (image.height >= maxHeight) {
+        // 縦比率にイメージの縦・横を合わせる
+        dh = maxHeight
+        dw = image.width * (dh / image.height)
+
+        // canvasの大きさを指定
+        canvas.width = dw
+        canvas.height = dh
+
+    } 
+    if (image.width >= maxWidth) {
+        // 横比率にイメージの縦・横を合わせる
+        dw = maxWidth
+        dh = image.height * (maxWidth / image.width)
+
+        // canvasの大きさを指定
+        canvas.height = dh
+        canvas.width = dw
+    }
+
+    // サムネイルに画像を描画
+    ctx.drawImage(image, 0, 0, image.width, image.height, 0, 0, dw, dh)
 }
 
