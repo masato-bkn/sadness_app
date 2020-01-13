886526bee3c8bc47bb925b43769446517e562bce
重田 将人 <masato.0928.j@gmail.com>
2020-01-14 08:29:44 +0900

    [add] バックエンドのテストコード作成 #90

---
+++ backend/sadness_app/emotion/tests/factories.py
@@ -0,0 +1,43 @@
+# coding: utf-8
+import factory
+from django.utils import timezone
+from emotion import models
+
+
+class AppUserFactory(factory.django.DjangoModelFactory):
+    """
+    AppUser Factory
+    """
+
+    # firebase認証で発行されたID
+    id = 1
+    # ユーザー名
+    username = "test user"
+    # 登録日時
+    created_at = timezone.now()
+    # 更新日時
+    updated_at = timezone.now()
+
+    class Meta:
+        model = models.AppUser
+
+class ImageFactory(factory.django.DjangoModelFactory):
+    """
+    Image Factory
+    """
+
+    # ファイル名
+    name = "test.png"
+    # 得点
+    score = 50
+    # コメント
+    comment = "test"
+    # ユーザー
+    user = factory.SubFactory(AppUserFactory)
+    # 登録日時
+    created_at = timezone.now()
+    # 更新日時
+    updated_at = timezone.now()
+
+    class Meta:
+        model = models.Image
